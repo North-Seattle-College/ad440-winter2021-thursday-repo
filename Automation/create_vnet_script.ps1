2b333108f7d1008d9309617da4d4b5b8d20fe836
unknown <lyaholkovsky@gmail.com>
2021-01-10 21:16:54 -0800

    edit to deployer.py - trouble with imports

--- Automation/create_vnet_script.ps1
+++ Automation/create_vnet_script.ps1
@@ -1,1 +1,4 @@
 # https://github.com/Azure-Samples/resource-manager-python-template-deployment
+# https://docs.microsoft.com/en-us/azure/templates/microsoft.resources/deploymentscripts
+# example: https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/deployment-script/deploymentscript-helloworld.ps1
+# https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/deployment-script-template
