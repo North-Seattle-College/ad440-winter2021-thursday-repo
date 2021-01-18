6c4ce9b9d8a643bc62e47163286cc1267ed6f956
Jenny <jennykunte90@gmail.com>
2021-01-18 15:21:50 -0800

    retrieve correct data from database for usersId

--- functions/HttpTriggerAPIUsersId/__init__.py
+++ functions/HttpTriggerAPIUsersId/__init__.py
@@ -2,11 +2,11 @@ import logging
 import pyodbc
 import os
 import azure.functions as func
+import json
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
+    data = ''
     db_server = os.environ["ENV_DATABASE_SERVER"]
     db_name = os.environ["ENV_DATABASE_NAME"]
     db_username = os.environ["ENV_DATABASE_USERNAME"]
@@ -18,22 +18,33 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     id = req.params.get('id')
     if not id:
         return func.HttpResponse(
-            "not found.",
-            status_code=404
+            "issue",
+            status_code=400
         )
     else:
         # query database
-
         with pyodbc.connect(connectionString) as conn:
 
             with conn.cursor() as cursor:
                 cursor.execute(
                     "SELECT * FROM users WHERE userId={}".format(id))
-                row = cursor.fetchone()
-                while row:
-                    print(str(row[0]) + " " + str(row[1]))
-                    row = cursor.fetchone()
-        return func.HttpResponse(
-            "Data goes here",
-            status_code=200
-        )
+
+                columns = [column[0] for column in cursor.description]
+                data = dict(zip(columns, cursor.fetchone()))
+
+                if not data:
+                    return func.HttpResponse(
+                        "not found.",
+                        status_code=404
+                    )
+                else:
+                    return func.HttpResponse(
+                        json.dumps(data),
+                        status_code=201,
+                        mimetype="application/json"
+                    )
+                    # return func.HttpResponse(
+                    #     json.dumps({"userId": row.userId, "firstName": row.firstName,
+                    #                 "lastName": row.lastName, "email": row.email, "deleted": row.deleted}),
+                    #     status_code=201
+                    # )
