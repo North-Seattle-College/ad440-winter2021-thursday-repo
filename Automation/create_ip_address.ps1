c2a563834bd21f26afbe146423057a71a655e51a
natni <natashsaya@gmail.com>
2021-01-18 23:58:33 -0800

    Added resource group & location validation.

--- Automation/create_ip_address.ps1
+++ Automation/create_ip_address.ps1
@@ -1,6 +1,6 @@
 ## Command to create a public ip ##
 
-# Take a user input 
+# Take parameter used to create Public IP Address
 param(
         [string] [Parameter(Mandatory=$true)] $rgname, # Name of the resource group
         [string] [Parameter(Mandatory=$true)] $location, # Location of Azure resources
@@ -9,5 +9,22 @@ param(
         [string] [Parameter(Mandatory=$true)] $allocation # AllocationMethod, Static or Dynamic
       )
 
-# Creates a new Public IP Address based on the params.
-New-AzPublicIpAddress -ResourceGroupName $rgname -Name $publicIp -Location $location -AllocationMethod $allocation -SKU $sku
+      # Check if resource group exists
+      if (!(Get-AzResourceGroup $rgname -ErrorAction SilentlyContinue)){ 
+        Write-Output "The resource group $rgname doesn't exist!"
+      }
+      else{        
+        $loc = Get-AzResourceGroup -Name $rgname | select-object -expandproperty location
+        
+        # Checks if the Public IP Address location matches the resource group's location 
+        if($loc.equals($location)){
+          # Creates a new Public IP Address based on the params.
+          New-AzPublicIpAddress -ResourceGroupName $rgname -Name $publicIp -Location $location -AllocationMethod $allocation -SKU $sku
+          Write-Output "Successfully created public ip address named $publicIp for the resource group $rgname"
+        } 
+        else{
+          Write-Output "Make sure you entered the right location. Location should be $loc"
+        }
+        
+      }
+
