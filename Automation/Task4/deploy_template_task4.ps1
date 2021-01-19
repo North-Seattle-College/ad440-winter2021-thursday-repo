feef0e709ac43e2306d588faadcff2c717c4ad83
ahmadafrasiyab <afrasiyab.ahmad@hotmail.com>
2021-01-18 21:32:46 -0800

    updated script

--- Automation/Task4/deploy_template_task4.ps1
+++ Automation/Task4/deploy_template_task4.ps1
@@ -21,12 +21,30 @@ param(
     $ResourceGroupName
 
 )
-$Credential = Get-Credential
+#$Credential = Get-Credential
 #logging in
-Connect-AzAccount  -Credential $Credential -Tenant $TenantId -ServicePrincipal
-#Get-AzContext -ListAvailable
-# Select-AzContext
+#Select-AzContext
+#Connect-AzAccount -Credential $Credential -Tenant $TenantId -ServicePrincipal
+#Connect-AzAccount   -Credential $Credential -Tenant $TenantId -ServicePrincipal
 # #creating a new resource group
 # #New-AzResourceGroup
 # #creating a new resource group deployment
 # #New-AzResourceGroupDeployment
+
+Connect-AzAccount -Tenant $TenantId -SubscriptionId $SubscriptionId;
+
+Write-Host "The Resource group will have the name in this format : [project]-[resource-type]-[environment]-[location]-[other-stuff]"
+$project = Read-Host "Please enter the project name"
+$resourcetype = Read-Host "Please enter the resource type"
+$environment = Read-Host "Please enter evironment"
+$location = Read-Host "Please enter location"
+$otherstuff = Read-Host "Please enter otherstuff"
+$name = $project + '-' + $resourcetype + '-' + $environment + '-' + $location + '-' + $otherstuff; 
+Write-Host "The name used will be" $name;
+
+$location = Read-Host "Please enter a location for your resource group"
+
+New-AzResource -Name $name -Location $location
+
+New-AzResourceGroupDeployment 
+
