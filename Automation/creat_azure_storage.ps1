a7891402337a0b3c838ffe547f336494a2c3b5d9
jinudean <jinudean@gmail.com>
2021-01-20 00:06:12 -0800

    Update creat_azure_storage.ps1
    update test for template

--- Automation/creat_azure_storage.ps1
+++ Automation/creat_azure_storage.ps1
@@ -15,3 +15,8 @@ $storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName `
 
 # Retrieve the context.
 $ctx = $storageAccount.Context
+
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$templateUri = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-storage-account-create/azuredeploy.json"
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -Location $location
