0f3486f375ae1cee85008c973deaef84cfbd6378
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2021-01-20 15:44:50 -0800

    updated code with a connection string to correspond to group's style, encapsuled db connection block in its own function

--- functions/HttpTriggerAPIUsersIdTaskId/__init__.py
+++ functions/HttpTriggerAPIUsersIdTaskId/__init__.py
@@ -3,19 +3,41 @@ import os
 import pyodbc
 import azure.functions as func
 
+#api methods block/placeholder for next sprint
 def get(param1, param2):
-    return func.HttpResponse(f'You passed in {param1}, {param2} values')
+    print ('You selected GET method with {param1}, {param2} values')
 def post(param):
-    return func.HttpResponse(f'You selected {param} method')
+    print ('You selected POST method with {param} values')
 def update(param1, param2):
-    return func.HttpResponse(f'You passed in {param1}, {param2} values')
+    print ('You selected UPDATE method with {param1}, {param2} values')
 def delete(param):
-    return func.HttpResponse(f'You selected {param} method')
+    print ('You selected POST method with {param} values')
+#connect to db function
+def connect():
+    try:
+        db_server = os.environ["ENV_DATABASE_SERVER"]
+        db_name = os.environ["ENV_DATABASE_NAME"]
+        db_username = os.environ["ENV_DATABASE_USERNAME"]
+        db_password = os.environ["ENV_DATABASE_PASSWORD"]
+        driver = '{ODBC Driver 17 for SQL Server}'
+        conn_string = "Driver={};Server={};Database={};Uid={};Pwd={};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;".format(
+        driver, db_server, db_name, db_username, db_password)
 
+# when passwords are permitted be saved to the config of the Azure function app, 
+# using a single connection string value, saved to the config of the app, 
+# is more economical : <conn_string = os.getenv('SQL_CONNECTION_STRING')>, get the string
+# from "Connection strings" menu on the left, in the database of interest        
+ 
+        cnxn = pyodbc.connect(conn_string)
+        return cnxn
+    except Exception:
+        print ("Unable to connect to db")
 
+#runs the main function
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-        
+
+#collects parameters passed in url        
     userId = req.params.get('userId')
     taskId = req.params.get('taskId')
     logging.info('Trying to get userId and taskId')
@@ -28,18 +50,19 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             userId = req_body.get('userId')
             taskId = req_body.get('taskId')
-
     if userId and taskId: 
         logging.info(f"Got userId:{userId} and taskId: {taskId}")
 
+#determines which API method was requested, and calls the API method
     method = req.method
     if not method:
         logging.critical('No method available')
         raise Exception('No method passed')
+
     if method == "GET":
         get(userId, taskId)
-        conn_string = os.getenv('SQL_CONNECTION_STRING')
-        cnxn = pyodbc.connect(conn_string)
+        #connects to db
+        cnxn = connect()
         cursor = cnxn.cursor()
         logging.info('opened connection')        
         logging.info('Going to execute a query')
@@ -53,9 +76,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             logging.info('Executed the query')         
             data = cursor.fetchall()
             logging.info(f"Got result: {data}")
-            return func.HttpResponse(f"This {method} method was called. You entered {userId} as uId and {taskId} as taskId. Result: {data}")
+            return func.HttpResponse(f"This {method} method was called. You entered {userId} as userId and {taskId} as taskId. Result: {data}")
         except Exception as e:
             return func.HttpResponse("Error: %s" % str(e), status_code=500)
+#irrespective of results, closes connection
         finally:
             cursor.close()
             cnxn.close()
@@ -63,11 +87,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if method == "POST":
         post(method)
-        # conn_string = os.getenv('SQL_CONNECTION_STRING')
-        # cnxn = pyodbc.connect(conn_string)
-        # cursor = cnxn.cursor()
-        # logging.info('opened connection')        
-        # logging.info('Going to execute a query')
+        # STARTER CODE FOR NEXT SPINT, SQL QUERY TESTED
         # try:
         #     #insert row into tasks table / create a new task
         #     #createdDate #'20120618 10:34:09 AM' and title/description are hardcoded for sprint1, update with automated date stamp and url params later
@@ -88,8 +108,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if method == "UPDATE":
         update(userId, taskId)
-        conn_string = os.getenv('SQL_CONNECTION_STRING')
-        cnxn = pyodbc.connect(conn_string)
+        #connects to db block
+        cnxn = connect()
         cursor = cnxn.cursor()
         logging.info('opened connection')        
         logging.info('Going to execute a query')
@@ -111,11 +131,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if method == "DELETE":
         delete(method)
-        # conn_string = os.getenv('SQL_CONNECTION_STRING')
-        # cnxn = pyodbc.connect(conn_string)
-        # cursor = cnxn.cursor()
-        # logging.info('opened connection')        
-        # logging.info('Going to execute a query')
+        # STARTER CODE FOR NEXT SPINT, SQL QUERY TESTED
         # try:
         #     # #delete row, client passes in userId and taskId
         #     sql_query = ("""DELETE FROM [dbo].[tasks] WHERE userId = ? AND taskId = ?""")
