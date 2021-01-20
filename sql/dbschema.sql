c9acfa4dcb1b5e4f6c5fbf2b5ee60ab305e864cc
Farhad <fbahrehmand82@gmail.com>
2021-01-19 18:13:25 -0800

    git comments

--- sql/dbschema.sql
+++ sql/dbschema.sql
@@ -3,7 +3,6 @@ CREATE TABLE users (
   firstName VARCHAR(50) NOT NULL,
   lastName VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  deleted INT DEFAULT 0 NOT NULL
 );
 
 CREATE TABLE tasks(
@@ -11,9 +10,10 @@ CREATE TABLE tasks(
   userId INT NOT NULL,
   title VARCHAR(50),
   description VARCHAR(400),
-  createdTime DATETIME NOT NULL,
-  deleted INT NOT NULL DEFAULT 0,
-  completed INT NOT NULL DEFAULT 0
+  createdDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  dueDate DATETIME NULL,
+  completed INT NOT NULL DEFAULT 0,
+  completedDate DATETIME NULL,
 
   CONSTRAINT FK_User_Task FOREIGN KEY (userId)
     REFERENCES  users(userId)
