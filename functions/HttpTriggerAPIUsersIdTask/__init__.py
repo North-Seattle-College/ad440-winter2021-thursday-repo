c0e18d60bcd389fde67e071321487a2ee79e6b03
ahmedmoussa <elyarmuk@hotmail.com>
2021-01-19 22:05:27 -0800

    initial code for task function

--- functions/HttpTriggerAPIUsersIdTask/__init__.py
+++ functions/HttpTriggerAPIUsersIdTask/__init__.py
@@ -1,24 +1,71 @@
 import logging
-
+import pyodbc
+import os
 import azure.functions as func
+import datetime
+import json
+from json import JSONEncoder
+
+
+# date1 = 'June 8, 2011'
+# date2 = '08/31/79'
+#  date3 = '09-04-1954 02:15:05'
+
+# date1_obj = datetime.strptime(date1, '%B %d, %Y')
+# date2_obj = datetime.strptime(date2, '%m/%d/%y')
+#  date3_obj = datetime.strptime(date3, '%m-%d-%Y %H:%M:%S')
+
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    # define the server and database names
+    server = os.environ["ENV_DATABASE_SERVER"]
+    database = os.environ["ENV_DATABASE_NAME"]
+    username = os.environ["ENV_DATABASE_USERNAME"]
+    password = os.environ["ENV_DATABASE_PASSWORD"]
+    
+    # define the connection string
+    driver = '{ODBC Driver 17 for SQL Server}'
+    connectionString = "Driver={};Server={};Database={};Uid={};Pwd={};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;".format(
+        driver, server, database, username, password)
+    # create the connection cursor
+    # cursor = conn.cursor()
+   
+    id = 1
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if not id:
+        return func.HTTPResponse('Bad Request', status_code=400 )
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        try:
+            with pyodbc.connect(connectionString) as conn:
+                return execute_query(conn, id)
+        except pyodbc.DatabaseError as e:
+            if e.args[0] == '28000':
+                return func.HttpResponse(
+                    "Unauthorized",
+                    status_code=403
+                )
+def execute_query(conn, id):
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "SELECT * FROM tasks WHERE userId={}".format(id))
+        row = cursor.fetchone()
+        if not row:
+            return func.HttpResponse(
+                "User not found",
+                status_code=404
+            )
+        else:
+            columns = [column[0] for column in cursor.description]
+            data = dict(zip(columns, row))
+            datetime = {'09-04-1954 02:15:05'}
+            return func.HttpResponse(
+                json.dumps(data),
+                json.dumps(datetime, default=str)
+                status_code=200,
+                mimetype="application/json"
+            )       
+ 
+
