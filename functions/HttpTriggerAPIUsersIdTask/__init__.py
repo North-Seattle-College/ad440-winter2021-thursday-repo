1b447c400390a4b1769d1be619c68ac56fceacb7
ahmedmoussa <elyarmuk@hotmail.com>
2021-01-20 21:17:35 -0800

    change the variables

--- functions/HttpTriggerAPIUsersIdTask/__init__.py
+++ functions/HttpTriggerAPIUsersIdTask/__init__.py
@@ -1,5 +1,5 @@
 import logging
-import pyodbc
+import pypyodbc
 import os
 import azure.functions as func
 import datetime
@@ -7,49 +7,51 @@ import json
 import time
 
 
-
 # to handle datetime with JSON
-def default(o):
-    if isinstance(o, (datetime.datetime, datetime.date)):
-        return o.isoformat()
+# It serialize datetime by converting it into string
+def default(dateHandle):
+    if isinstance(dateHandle, (datetime.datetime, datetime.date)):
+        return dateHandle.isoformat()
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    
 
     # define the server and database names
     server = os.environ["ENV_DATABASE_SERVER"]
     database = os.environ["ENV_DATABASE_NAME"]
     username = os.environ["ENV_DATABASE_USERNAME"]
     password = os.environ["ENV_DATABASE_PASSWORD"]
-    
+
     # define the connection string
     driver = '{ODBC Driver 17 for SQL Server}'
     cnxn = "Driver={};Server={};Database={};Uid={};Pwd={};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;".format(
         driver, server, database, username, password)
-    
-   
-    id = req.params.get('userId')
 
-    if not id:
-        return func.HTTPResponse('Bad Request', status_code=400 )
+    user_id = req.route_params.get('userId')
+
+    if not user_id:
+        return func.HttpResponse('Bad Request', status_code=400)
     else:
         try:
-            with pyodbc.connect(cnxn) as conn:
-                return tasks_query(conn, id)
-        except pyodbc.DatabaseError as err:
+            with pypyodbc.connect(cnxn) as conn:
+                return tasks_query(conn, user_id)
+        except pypyodbc.DatabaseError as err:
             if err.args[0] == '28000':
                 return func.HttpResponse(
                     "Unauthorized User",
-                    status_code=403 
+                    status_code=403
                 )
 
 # create the connection cursor
-def tasks_query(conn, id):
+
+
+def tasks_query(conn, user_id):
     # create the query
     with conn.cursor() as cursor:
         cursor.execute(
-            "SELECT * FROM tasks WHERE userId={}".format(id))
-        row = cursor.fetchone()
+            "SELECT * FROM tasks WHERE userId={}".format(user_id))
+        row = list(cursor.fetchall())
+        logging.info(row)
         if not row:
             return func.HttpResponse(
                 "User not found",
@@ -62,6 +64,4 @@ def tasks_query(conn, id):
                 json.dumps(data, default=default),
                 status_code=200,
                 mimetype="application/json"
-            )       
- 
-
+            )
