883954ab64d07e127c1cdd1c31a00ccc25dfa225
natni <natashsaya@gmail.com>
2021-01-20 22:29:53 -0800

    Fix on previous commit.

--- Automation/PublicIP/create_ip_address.ps1
+++ Automation/PublicIP/create_ip_address.ps1
@@ -2,15 +2,12 @@
 
 # Take parameter used to create Public IP Address
 param(
-        [string] [Parameter(Mandatory=$true)] $tenantId,          
-        [string] [Parameter(Mandatory=$true)] $applicationId, 
-        [string] [Parameter(Mandatory=$true)] $secret,            
-        [string] [Parameter(Mandatory=$true)] $subscriptionId,
+        [string] [Parameter(Mandatory=$true)] $tenantId, # Tenant ID found in the Key Vault     
+        [string] [Parameter(Mandatory=$true)] $applicationId, # Application ID found in the Key Vault  
+        [string] [Parameter(Mandatory=$true)] $secret, # Secret Key found in the Key Vault            
+        [string] [Parameter(Mandatory=$true)] $subscriptionId, # Subscription ID found in the Key Vault  
         [string] [Parameter(Mandatory=$true)] $rgname, # Name of the resource group
-        #[string] [Parameter(Mandatory=$true)] $location, # Location of Azure resources
         [string] [Parameter(Mandatory=$true)] $publicIp # Name of the azure public address
-        #[string] [Parameter(Mandatory=$true)] $sku, # Type of SKU, Standard or Basic
-        #[string] [Parameter(Mandatory=$true)] $allocation # AllocationMethod, Static or Dynamic
       )
 
       #Logs in and sets subscription      
@@ -22,22 +19,14 @@ param(
       if (!(Get-AzResourceGroup $rgname -ErrorAction SilentlyContinue)){ 
         Write-Output "The resource group $rgname doesn't exist!"
       }
-      else{        
+      else{  
+        
+        # Get the location of the resource group
         $loc = Get-AzResourceGroup -Name $rgname | select-object -expandproperty location
 
-        # Checks if the Public IP Address location matches the resource group's location 
-        #if($loc.equals($location)){
-
-          # Creates a public ip address in the given location
-          New-AzResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile $pathToPubIpTemplate -pubIpName $publicIp -location $loc
-          
           # Creates a new Public IP Address based on the params.
-          # New-AzPublicIpAddress -ResourceGroupName $rgname -Name $publicIp -Location $location -AllocationMethod $allocation -SKU $sku
-          Write-Output "Successfully created public ip address named $publicIp for the resource group $rgname"
-        #} 
-        #else{
-         # Write-Output "Make sure you entered the right location. Location should be $loc"
-       # }
+          New-AzResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile $pathToPubIpTemplate -pubIpName $publicIp -location $loc
         
+          Write-Output "Successfully created public ip address named "$publicIp" for the resource group "$rgname"" 
       }
 
