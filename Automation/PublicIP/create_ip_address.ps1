af0d843bd0c3446e3ae4dcf6f787a15b0547cff4
natni <natashsaya@gmail.com>
2021-01-21 19:00:37 -0800

    Updated the param rgname to more descriptive one.

--- Automation/PublicIP/create_ip_address.ps1
+++ Automation/PublicIP/create_ip_address.ps1
@@ -6,7 +6,7 @@ param(
         [string] [Parameter(Mandatory=$true)] $applicationId, # Application ID found in the Key Vault  
         [string] [Parameter(Mandatory=$true)] $secret, # Secret Key found in the Key Vault            
         [string] [Parameter(Mandatory=$true)] $subscriptionId, # Subscription ID found in the Key Vault  
-        [string] [Parameter(Mandatory=$true)] $rgname, # Name of the resource group
+        [string] [Parameter(Mandatory=$true)] $resourceGroupName, # Name of the resource group
         [string] [Parameter(Mandatory=$true)] $publicIp # Name of the azure public address
       )
 
@@ -16,17 +16,17 @@ param(
       $pathToPubIpTemplate = "./public_ip_template.json"
 
       # Checks if resource group exists
-      if (!(Get-AzResourceGroup $rgname -ErrorAction SilentlyContinue)){ 
-        Write-Output "The resource group $rgname doesn't exist!"
+      if (!(Get-AzResourceGroup $resourceGroupName -ErrorAction SilentlyContinue)){ 
+        Write-Output "The resource group $resourceGroupName doesn't exist!"
       }
       else{  
         
         # Get the location of the resource group
-        $loc = Get-AzResourceGroup -Name $rgname | select-object -expandproperty location
+        $loc = Get-AzResourceGroup -Name $resourceGroupName | select-object -expandproperty location
 
           # Creates a new Public IP Address based on the params.
-          New-AzResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile $pathToPubIpTemplate -pubIpName $publicIp -location $loc
+          New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $pathToPubIpTemplate -pubIpName $publicIp -location $loc
         
-          Write-Output "Successfully created public ip address named "$publicIp" for the resource group "$rgname"" 
+          Write-Output "Successfully created public ip address named "$publicIp" for the resource group "$resourceGroupName"" 
       }
 
