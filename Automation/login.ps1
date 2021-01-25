358d7c31095067c58679dc990a3517f373589154
natni <natashsaya@gmail.com>
2021-01-25 11:25:17 -0800

    Updated login.ps1 file

--- Automation/login.ps1
+++ Automation/login.ps1
@@ -5,10 +5,32 @@ param(
         [string] [Parameter(Mandatory=$true)] $subscriptionId
       )
 
-[securestring]$secureSecret = ConvertTo-SecureString $secret -AsPlainText -Force      
-[pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($applicationId, $secureSecret)
-Connect-AzAccount -ServicePrincipal -Credential $credObject -Tenant $tenantId      
-write-output "Logged into the account $applicationId"
+# If the Subscription Id is NOT null, the user is logged in
+$loggedIn = ((Get-AzContext).Tenant.Id -eq $tenantId)
 
-Set-AzContext -Subscription $subscriptionId
-write-output "Set to subscription $subscriptionId"
+if ($loggedIn) {
+  Write-Host("Already logged in")
+
+  # Check we're on the correct Subscription
+  $correctSub = (Get-AzContext).Subscription.Id -eq $subscriptionId
+  if ($correctSub) {
+    Write-Host("Correct subscription")
+  } else {
+    Write-Host("Wrong subscription. Logging out...")
+    Disconnect-AzAccount
+    $loggedIn = False
+  }
+
+} 
+if (!$loggedIn) {
+  # Log In
+  Write-Host("Logging in...")
+  [securestring]$secureSecret = ConvertTo-SecureString $secret -AsPlainText -Force      
+  [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($applicationId, $secureSecret)
+  Connect-AzAccount -ServicePrincipal -Credential $credObject -Tenant $tenantId      
+  Write-Host "Logged into the account $applicationId"
+
+  # Set Subscription
+  Set-AzContext -Subscription $subscriptionId
+  Write-Host "Set to subscription $subscriptionId"
+} 
