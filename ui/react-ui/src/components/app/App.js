06b6de0770c381dbdea94973555afe9048aae221
Farhad <fbahrehmand82@gmail.com>
2021-01-27 15:35:01 -0800

    add routes

--- ui/react-ui/src/components/app/App.js
+++ ui/react-ui/src/components/app/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'; 
 import UserTasks from '../routes/userTasks/UserTasks.js';
-
+import Home from '../routes/home/Home.js';
+ 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Route path='/' exact component={Home} />
         {/* Jak */}
         {/* <Route path='/users/' component={}/> */}
         {/* David */}
