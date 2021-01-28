2da976d309ad82854f7b9a66491ffad0bae30d91
Jenny <jennykunte90@gmail.com>
2021-01-27 20:47:01 -0800

    testCRUD

--- functions/HttpTriggerAPIUsersId/__init__.py
+++ functions/HttpTriggerAPIUsersId/__init__.py
@@ -11,6 +11,8 @@ import json
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    # TODO  error handleing like lenny
+
     db_server = os.environ["ENV_DATABASE_SERVER"]
     db_name = os.environ["ENV_DATABASE_NAME"]
     db_username = os.environ["ENV_DATABASE_USERNAME"]
@@ -19,8 +21,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     connectionString = "Driver={};Server={};Database={};Uid={};Pwd={};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;".format(
         driver, db_server, db_name, db_username, db_password)
 
-    # This gets the userId from the query string
+   # This gets the userId from the query string
     id = req.route_params.get('userId')
+
     if not id:
         # If no userId is supplied http response -> Bad Request
         return func.HttpResponse(
@@ -30,18 +33,41 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     else:
         try:
             with pypyodbc.connect(connectionString) as conn:
-                return get_user_by_id(conn, id)
+
+                if req.method == 'GET':
+                    return get(req, conn, id)
+                elif req.method == 'POST':
+                    return post(req, conn, id)
+                elif req.method == 'PUT':
+                    return put(req, conn, id)
+                elif req.method == 'PATCH':
+                    return patch(req, conn, id)
+                elif req.method == 'DELETE':
+                    return delete(req, conn, id)
+
+                # Close connection after method call
+                conn.close()
+                logging.info('Closed the connection')
+
         except pypyodbc.DatabaseError as e:
-            # This code is returned by pypyodbc meaning Unautroized when a bad password is supplied etc
+            # 28000 - This code is returned by pypyodbc meaning Unautroized when a bad password is supplied etc
+            # 42000 - Azure firewall blocks ip
             # Adddtional error handeling will need to be implemented, maybe in srpint 2
-            if e.args[0] == '28000':
+
+            # TODO ADD LOGGING with error detail
+            if e.args[0] == '28000' or e.args[0] == '42000':
                 return func.HttpResponse(
                     "Unauthorized",
                     status_code=403
                 )
+        else:
+            return func.HttpResponse(
+                "Error",
+                status_code=400
+            )
 
 
-def get_user_by_id(conn, id):
+def get(req, conn, id):
     with conn.cursor() as cursor:
         cursor.execute(
             "SELECT * FROM users WHERE userId={}".format(id))
@@ -57,9 +83,73 @@ def get_user_by_id(conn, id):
             # https://stackoverflow.com/questions/16519385/output-pyodbc-cursor-results-as-python-dictionary/16523148#16523148
             columns = [column[0] for column in cursor.description]
             data = dict(zip(columns, row))
+            cursor.close()
 
             return func.HttpResponse(
                 json.dumps(data),
                 status_code=200,
                 mimetype="application/json"
             )
+
+
+def post(req, conn, id):
+    return func.HttpResponse(
+        "Method not allowed!",
+        status_code=405
+    )
+
+
+def put(req, conn, id):
+    user_req_body = req.get_json()
+    try:
+        assert "firstName" in user_req_body, "New user request body did not contain field: 'firstName'"
+        assert "lastName" in user_req_body, "New user request body did not contain field: 'lastName'"
+        assert "email" in user_req_body, "New user request body did not contain field: 'email'"
+    except AssertionError as user_req_body_content_error:
+        logging.error(
+            "New user request body did not contain the necessary fields!")
+        return func.HttpResponse(user_req_body_content_error.args[0], status_code=400)
+
+    with conn.cursor() as cursor:
+        # Unpack user data
+        firstName = user_req_body["firstName"]
+        lastName = user_req_body["lastName"]
+        email = user_req_body["email"]
+        user_params = (firstName, lastName, email)
+
+        # Update user  query
+        update_user_query = "UPDATE users SET firstName = {}, lastName = {}, email = {} WHERE userId={}".format(
+            firstName, lastName, email, id)
+
+        cursor.execute(update_user_query)
+
+
+def patch(req, conn, id):
+    return func.HttpResponse(
+        "Method not allowed!",
+        status_code=405
+    )
+
+
+def delete(req, conn, id):
+    return ""
+# Implement Response codes:
+#  responses:
+#         "200":
+#           description: "User Deleted Successfully"
+#         "400":
+#           description: "Bad Request"
+#         "401":
+#           description: "Unauthorized"
+#         "403":
+#           description: "Forbidden"
+#         "404":
+#           description: "User Not Found"
+#         "429":
+#           description: "Too Many Requests"
+#         "500":
+#           description: "Internal Server Error"
+#         "502":
+#           description: "Bad Gateway"
+#         "503":
+#           description: "Service Unavailable"
