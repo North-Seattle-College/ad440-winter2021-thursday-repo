3dd1ffc510400a2df437b8f1ba63804d86da6e06
Toddy Mladenov <toddysm@agitaretech.com>
2021-01-28 19:00:18 -0800

    Updated the examples

--- example/functions-example/HttpTriggerToddysmExample/__init__.py
+++ example/functions-example/HttpTriggerToddysmExample/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import pyodbc
 
 import azure.functions as func
 
@@ -7,8 +8,18 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    db_username = os.environ["ENV_DATABASE_USERNAME"]
-    db_password = os.environ["ENV_DATABASE_PASSWORD"]
+    #db_username = os.environ["ENV_DATABASE_USERNAME"]
+    #db_password = os.environ["ENV_DATABASE_PASSWORD"]
+
+    conn_string = "Driver={ODBC Driver 17 for SQL Server};Server=tcp:nsc-sqlsrv-usw2-sqltest.database.windows.net,1433;Database=nsc-sqldb-usw2-sqltest;Uid=sqladmin;Pwd=Wm27H#$%imy5dnTkWMYWlXhTy91O;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+
+    with pyodbc.connect(conn_string) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT taskId, taskUserId, title, taskDescription, completed FROM Tasks")
+            rows = cursor.fetchall()
+        tasks = []
+        for row in rows:
+            tasks.append(list(row))
 
     name = req.params.get('name')
     if not name:
