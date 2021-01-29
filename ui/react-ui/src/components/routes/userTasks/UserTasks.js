71020fa5472062401935037db321a7bbcb1cc838
Farhad <fbahrehmand82@gmail.com>
2021-01-29 15:55:06 -0800

    update url

--- ui/react-ui/src/components/routes/userTasks/UserTasks.js
+++ ui/react-ui/src/components/routes/userTasks/UserTasks.js
@@ -8,7 +8,8 @@ var UserTasks = () => {
   var {userId} = useParams();
 
   useEffect(() => {
-    fetch(`https://nsc-functionsapp-team1.azurewebsites.net/api/${userId}/tasks?`)
+    // url should be replaced with => https://nsc-functionsapp-team1.azurewebsites.net/api/${userId}/tasks?
+    fetch(`https://w21httptriggernataliatest.azurewebsites.net/api/${userId}/tasks?`)
     .then(response => response.json())
     .then(data => setUserTasks(data))
     .catch((error) => console.error(error))
@@ -18,10 +19,14 @@ var UserTasks = () => {
 
   return (
     <div className='user-tasks-container'>
-      <BootstrapTable 
-        heatherItems={userTasks.length > 0 ? Object.keys(userTasks[0]) : []}
-        rows={userTasks.length > 0 ? userTasks : []}
-      />
+      <h3>all tasks for user with userId {userId}</h3>
+      {userTasks.length > 0 ? 
+        <BootstrapTable 
+          heatherItems={userTasks.length > 0 ? Object.keys(userTasks[0]) : []}
+          rows={userTasks.length > 0 ? userTasks : []}
+        /> :
+        <h4>there is no task for this user</h4>
+      }
     </div>
   )
 }
