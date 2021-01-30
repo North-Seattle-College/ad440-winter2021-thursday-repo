9af837edc253fa9489358acdc1b946a648bfe6f4
natni <44041437+natni@users.noreply.github.com>
2021-01-30 01:05:03 -0800

    React UI that displays all users from Amazon S3 Bucket

--- ui/react-ui/src/App.js
+++ ui/react-ui/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Users from './Pages/Users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+        <Router>
+          <div>
+            <p>All Users</p>
+            <Route path='/allUsers' exact component={Users}></Route>
+          </div>
+        </Router>
+
+    )   
+    }
 }
 
 export default App;
