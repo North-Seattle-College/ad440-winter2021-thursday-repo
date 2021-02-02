64114e6dce21ff8211650f955519e08dc3acf2e5
unknown <lyaholkovsky@gmail.com>
2021-02-01 21:25:20 -0800

    updated parameter names to conform to conventions

--- Automation/Login/Login.psm1
+++ Automation/Login/Login.psm1
@@ -1,20 +1,20 @@
 function Login
 {
     param(
-          [string] [Parameter(Mandatory=$true)] $tenantId,
-          [string] [Parameter(Mandatory=$true)] $applicationId,
-          [string] [Parameter(Mandatory=$true)] $secret,
-          [string] [Parameter(Mandatory=$true)] $subscriptionId
+          [string] [Parameter(Mandatory=$true)] $TenantId,
+          [string] [Parameter(Mandatory=$true)] $SPApplicationId,
+          [string] [Parameter(Mandatory=$true)] $SPSecret,
+          [string] [Parameter(Mandatory=$true)] $SubscriptionId
         )
 
   # If the Subscription Id is NOT null, the user is logged in
-  $loggedIn = ((Get-AzContext).Tenant.Id -eq $tenantId)
+  $loggedIn = ((Get-AzContext).Tenant.Id -eq $TenantId)
 
   if ($loggedIn) {
     Write-Host("Already logged in")
 
     # Check we're on the correct Subscription
-    $correctSub = (Get-AzContext).Subscription.Id -eq $subscriptionId
+    $correctSub = (Get-AzContext).Subscription.Id -eq $SubscriptionId
     if ($correctSub) {
       Write-Host("Correct subscription")
     } else {
@@ -27,14 +27,14 @@ function Login
   if (!$loggedIn) {
     # Log In
     Write-Host("Logging in...")
-    [securestring]$secureSecret = ConvertTo-SecureString $secret -AsPlainText -Force      
-    [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($applicationId, $secureSecret)
-    Connect-AzAccount -ServicePrincipal -Credential $credObject -Tenant $tenantId      
-    Write-Host "Logged into the account $applicationId"
+    [securestring]$secureSecret = ConvertTo-SecureString $SPSecret -AsPlainText -Force      
+    [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($SPApplicationId, $secureSecret)
+    Connect-AzAccount -ServicePrincipal -Credential $credObject -Tenant $TenantId      
+    Write-Host "Logged into the account $ApplicationId"
 
     # Set Subscription
-    Set-AzContext -Subscription $subscriptionId
-    Write-Host "Set to subscription $subscriptionId"
+    Set-AzContext -Subscription $SubscriptionId
+    Write-Host "Set to subscription $SubscriptionId"
   } 
 }
 Export-ModuleMember -Function 'Login'
