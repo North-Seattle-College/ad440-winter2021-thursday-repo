dd65275062c37d5cde339f9696bcb5bc2a483cda
Farhad <fbahrehmand82@gmail.com>
2021-02-02 09:27:34 -0800

    conflicts

--- ui/react-ui/src/components/routes/userid/userid.js
+++ ui/react-ui/src/components/routes/userid/userid.js
@@ -10,7 +10,7 @@ var User = () => {
   var {userId} = useParams();
 
   useEffect(() => {
-    fetch(`https://nsc-functionsapp-team1.azurewebsites.net/api/users/${userId}`)
+    fetch(`https://nsc-functionsapp-team1.azurewebsites.net/api/users/${userId}?`)
       .then(response => response.json())
       .then(data => setUser(data))
       .catch((error) => console.error(error))
