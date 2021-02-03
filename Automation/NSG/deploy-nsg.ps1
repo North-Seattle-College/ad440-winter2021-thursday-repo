5cbac230947ff83f97598dacb0bb8665e22e41ca
Jack <jackb116@aol.com>
2021-02-03 11:38:41 -0800

    adding error handling for cmdlets, and larissa's login module

--- Automation/NSG/deploy-nsg.ps1
+++ Automation/NSG/deploy-nsg.ps1
@@ -1,5 +1,11 @@
 param(
-    $TemplateFilePath = "./template.json",
+    # Default path, can be overwritten if custom path is provided
+    $DefaultTemplateFilePath =  "./template.json",
+
+    # Used for overwriting default path
+    [Parameter(Mandatory=$False)]
+    [string]
+    $TemplateFilePath,
 
     # Your tenant Id in Azure
     [Parameter(Mandatory=$True)]
@@ -9,12 +15,12 @@ param(
     # Found in Azure secrets as appId
     [Parameter(Mandatory=$True)]
     [string]
-    $ServicePrincipalUsername,
+    $SPApplicationId,
 
     # Found in Azure secrets as secret
     [Parameter(Mandatory=$True)]
     [string]
-    $ServicePrincipalSecret,
+    $SPSecret,
 
     # Your tenant Id in Azure
     [Parameter(Mandatory=$True)]
@@ -100,7 +106,13 @@ param(
     $DestinationAddressPrefies
 )
 
-$params = @{
+# If template path provded, do not use default
+if ($TemplateFilePath) {
+    $DefaultTemplateFilePath = $TemplateFilePath
+}
+
+# Hash table for template deployment
+$templateParams = @{
     tags = @{
         "ownerEmail" = $OwnerEmail
     }
@@ -122,6 +134,7 @@ $params = @{
         "sourceAddressPrefixes" = $SourceAddressPrefixes
         "destinationAddressPrefixes" = $DestinationAddressPrefies
     }
+
 # Example SSH security rule
 <#
         "name" = SSH_Access;
@@ -139,15 +152,34 @@ $params = @{
         "destinationAddressPrefixes"= "";
 #>
 }
-# Logs in and sets subscription      
-& "../login.ps1" $TenantId $ServicePrincipalUsername $ServicePrincipalSecret $SubscriptionId
 
-# Creates/Updates resource group
-New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Force
+# Login using Larissa's Login module & contains error handling)
+Import-Module ..\Login
+Login $TenantId $SPApplicationId $SPSecret $SubscriptionId
 
-# Deploy with template + parameters
-New-AzResourceGroupDeployment `
-    -Name $DeploymentName `
-    -ResourceGroupName $ResourceGroupName `
-    -TemplateFile $TemplateFilePath `
-    -TemplateParameterObject $params -Verbose
+# Error handling for general issues + resource group creation/update
+If (New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Force) {
+    Write-Host -ForegroundColor Blue "`nResource Group created/updated successfully.`n"
+} Else {
+    Write-Host -ForegroundColor Red $_.Exception.Message
+    Write-Host -ForegroundColor Yellow "`nError creating/updating resource group. See above error message(s). Please double check all entered parameters and try again.`n"
+}
+
+# Error handling for general issues & nsg resource group deployment
+
+# Check if NSG already exists
+If (Get-AzNetworkSecurityGroup -Name $SecurityGroupName -ResourceGroupName $ResourceGroupName -ErrorAction Ignore) {
+    Write-Host -ForegroundColor Red "`nNetwork Security Group already exists, please try again with a different SecurityGroupName.`n"
+} Else {
+    # Deploy with template + parameters
+    If (New-AzResourceGroupDeployment -Name $DeploymentNames -ResourceGroupName $ResourceGroupName -TemplateFile $DefaultTemplateFilePath -TemplateParameterObject $templateParams -Verbose) {
+        Write-Host -ForegroundColor Blue "`nNSG created successfully.`n"
+    } Else {
+        Write-Host -ForegroundColor Red $_.Exception.Message
+        Write-Host -ForegroundColor Yellow "`nError deploying resources. See above error message(s).Please double check all entered parameters and try again.`n"
+    }
+}
+
+<# 
+TODO: Look into creating multiple security rules
+#>
