c36baf434f728f83fcfc6eb4477d095389bd119c
kevinles117 <kevinles117@yahoo.com>
2021-02-05 17:41:57 -0800

    deleted unncessary files

--- Automation/Task3/task_3_script.ps1
+++
@@ -1,6 +0,0 @@
-$resourceGroupName = $args[0]
-$location = $args[1]
-$templateUri = $args[2]
-
-New-AzResourceGroup -Name $resourceGroupName -Location $location
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri
