458779fccdbe48361dcf0a28ab1bc3f1be42d88d
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2021-02-13 15:35:42 -0800

    added server name input

--- Automation/SQL/azureDeploy.ps1
+++ Automation/SQL/azureDeploy.ps1
@@ -1,21 +1,6 @@
 # Creates resource group and Azure SQL logical server
 
 param(
-<<<<<<< HEAD
-        # Login parameters
-        [string] [Parameter(Mandatory=$true)] $tenantId,          
-        [string] [Parameter(Mandatory=$true)] $applicationId,
-        [string] [Parameter(Mandatory=$true)] $secret,
-        [string] [Parameter(Mandatory=$true)] $subscriptionId,
-        # Azure SQL server parameters
-        [string] [Parameter(Mandatory=$true)] $location,
-        [string] [Parameter(Mandatory=$true)] $resourceGroupName,
-        [string] [Parameter(Mandatory=$true)] $serverName,
-        [string] [Parameter(Mandatory=$true)] $administratorLogin,
-        [string] [Parameter(Mandatory=$true)] $administratorLoginPassword
-)
-
-=======
     # Login parameters
     [string] [Parameter(Mandatory=$true)] $tenantId,          
     [string] [Parameter(Mandatory=$true)] $applicationId,
@@ -29,11 +14,10 @@ param(
     [string] [Parameter(Mandatory=$true)] $administratorLoginPassword,
     # Azure SQL db parameters
     
-    # Tag paramenters
+    # Tag parameters
     [string] [Parameter(Mandatory=$true)] $createdBy,
     [string] [Parameter(Mandatory=$true)] $creatorsEmail              
 )
->>>>>>> d82162f... added interactive tags
 [securestring] $administratorLoginPassword = ConvertTo-SecureString $administratorLoginPassword -AsPlainText -Force
 
 # Log in and set the SubscriptionId in which to create these objects
@@ -51,22 +35,14 @@ if (!$resourceGroupExists) {
         New-AzResourceGroup -Name $resourceGroupName -Location "$location" -Force
         Write-Host "Created resource group $resourceGroupName"
 } else {
-<<<<<<< HEAD
-        Write-Host "Resource group already exists."
-=======
         Write-Host "Resource group $resourceGroupName already exists."
->>>>>>> d82162f... added interactive tags
 }
 
 # Deploy template
 Write-host "Creating primary server..."
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
 -TemplateFile $pathToAzSqlTemplate -administratorLogin $administratorLogin `
-<<<<<<< HEAD
--administratorLoginPassword $administratorLoginPassword
-=======
--administratorLoginPassword $administratorLoginPassword -createdBy $createdBy -creatorsEmail $creatorsEmail
->>>>>>> d82162f... added interactive tags
+-administratorLoginPassword $administratorLoginPassword -serverName $serverName -location $location -createdBy $createdBy -creatorsEmail $creatorsEmail
 
 
 # Clear deployment 
