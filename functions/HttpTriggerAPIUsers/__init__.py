0dafe5ec3c34f2ac69e042b8bf77f41d7853cbfc
Farhad <fbahrehmand82@gmail.com>
2021-02-14 22:53:39 -0800

    update functions urls

--- functions/HttpTriggerAPIUsers/__init__.py
+++ functions/HttpTriggerAPIUsers/__init__.py
@@ -4,6 +4,7 @@ import logging
 import os
 import azure.functions as func
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info(
         'Python HTTP trigger for /users function is processing a request.')
