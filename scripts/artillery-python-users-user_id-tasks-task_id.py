20c64510eaf8c63e3762ac459b1d4edd2810d2d1
Jack <jackb116@aol.com>
2021-02-16 19:16:46 -0800

    script ready for merge test

---
+++ scripts/artillery-python-users-user_id-tasks-task_id.py
@@ -0,0 +1,42 @@
+import boto3
+import sys
+import logging
+from botocore.exceptions import ClientError
+
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html 
+
+def main():
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :return: True if file was uploaded, else False
+    """
+
+    # Variables from shell script
+    bucket = sys.argv[1]
+    aws_access_key = sys.argv[2]
+    aws_access_secret = sys.argv[3]
+    file_name = sys.argv[4]
+
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_access_secret,
+    )
+
+    s3_client = session.client('s3')
+
+    try:
+        response = s3_client.upload_file(
+            Filename = file_name,
+            Bucket = bucket,
+            Key = file_name # Object name default to file name
+        )
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+main()
+
---
+++ scripts/artillery-shell-users-user_id-tasks-task_id.sh
@@ -0,0 +1,13 @@
+bucket_name=$1
+aws_access_key=$2
+aws_access_secret=$3
+local_path=$4
+
+# # Create a zip of the current directory.
+# zip -r $local_path . -x .git/ .git/*** .github/workflows/artillery-report-users-user_id-tasks-task_id.yaml scripts/artillery-shell-users-user_id-tasks-task_id.sh scripts/artillery-python-users-user_id-tasks-task_id.py .DS_Store
+
+# Install required dependencies for Python script.
+pip3 install boto3
+
+# Run upload script
+python3 scripts/artillery-python-users-user_id-tasks-task_id.py $bucket_name $aws_access_key $aws_access_secret $local_path
