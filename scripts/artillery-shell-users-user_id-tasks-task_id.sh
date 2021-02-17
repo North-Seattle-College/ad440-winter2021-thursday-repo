7e56cb7aadb3f21901b61f96191cced77734a9c9
Jack <jackb116@aol.com>
2021-02-16 19:58:51 -0800

    commenting out, adding sources

--- scripts/artillery-shell-users-user_id-tasks-task_id.sh
+++ scripts/artillery-shell-users-user_id-tasks-task_id.sh
@@ -3,6 +3,8 @@ aws_access_key=$2
 aws_access_secret=$3
 local_path=$4
 
+# https://keithweaver.ca/lessons/uploading-files-to-aws-s3-with-github-actions?s=yktc
+
 # # Create a zip of the current directory.
 # zip -r $local_path . -x .git/ .git/*** .github/workflows/artillery-report-users-user_id-tasks-task_id.yaml scripts/artillery-shell-users-user_id-tasks-task_id.sh scripts/artillery-python-users-user_id-tasks-task_id.py .DS_Store
 
