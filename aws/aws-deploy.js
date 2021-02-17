b8d8e6530b5894fb75a25b64700835ee9e8c975e
Farhad <fbahrehmand82@gmail.com>
2021-02-17 15:27:13 -0800

    add

--- aws/aws-deploy.js
+++ aws/aws-deploy.js
@@ -1,19 +1,18 @@
 const AWS = require("aws-sdk");
 const fs = require("fs");
-// configuration
+
 const config = {
   s3BucketName: process.env.BUCKET_NAME,
   filePath: `../test/results/serverless-artillery-test-users-api.json`
 };
-// serverless-artillery-test-users-api.json
-// initialize S3 client
+
 const s3Config = {
     signatureVersion: 'v4',
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 }
 const s3 = new AWS.S3(s3Config);
-console.log('sdasdasdasdasdasdasdasdasdsad', s3)
+
 s3.putObject({
   Bucket: 'usersapitest',
   Key: 'serverless-artillery-test-users-api.json',
