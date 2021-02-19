f6cfe86e334e867e8009e0d4b07985e7db01d4dc
kevinles117 <kevinles117@yahoo.com>
2021-02-18 16:59:16 -0800

    Removed any workflow or artillery test files that weren't mine

--- test/artillery/artillery-functions.js
+++
@@ -1,9 +0,0 @@
-function logResponse(requestParams, response, context, ee, next){
-      console.log(`${response.request.uri.path}: ${response.statusCode}`);
-      return next();
-  }
-
-module.exports = {
-    logResponse: logResponse
-}
-
