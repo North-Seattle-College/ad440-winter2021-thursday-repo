cfb3d536751e53e144fe2c6d93b1a3a70f95adb3
Lenny Brown <penn2014@gmail.com>
2021-03-02 17:01:17 -0800

    Improved logging and readability just a bit

--- test/ui/ui-tests-user.js
+++ test/ui/ui-tests-user.js
@@ -1,18 +1,12 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-    const uiLocation = "https://nscstrdevusw2thucommon.z5.web.core.windows.net/";
+    const uiLocation = "https://nscstrdevusw2thucommon.z5.web.core.windows.net/users";
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     console.log(`Page opened, going to ${uiLocation}`);
     await page.goto(uiLocation)
 
-    const [div] = await page.$x("//div[contains(., '/users')]");
-    await div.click();
-    // const [response] = await Promise.all([
-    //     page.waitForNavigation(),
-    //     div.click(),
-    // ]);
     let [month, date, year]    = new Date().toLocaleDateString("en-US").split("/")
     let [hour, minute, second, period] = new Date().toLocaleTimeString("en-US").split(/:| /)
 
@@ -25,8 +19,11 @@ const puppeteer = require("puppeteer");
     if (String(minute).length == 1) minute = "0" + minute;
     if (String(second).length == 1) minute = "0" + second;
 
-    todate = `${year}_${month}_${date}_${hour}-${minute}-${second}`;
-    await page.screenshot({ path: `../results/ui-test-results/users/ui_users_${todate}.png`});
+    const todate = `${year}_${month}_${date}_${hour}-${minute}-${second}`;
+    const screenshotPath = `../results/ui-test-results/users/ui_users_${todate}.png`;
+
+    console.log(`Navigated to ${uiLocation}, taking screenshot and storing in ${screenshotPath}`);
+    await page.screenshot({ path: screenshotPath});
 
     await browser.close();
 })(); 
