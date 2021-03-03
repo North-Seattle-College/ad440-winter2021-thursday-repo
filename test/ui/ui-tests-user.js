86122491ff8751e5a3dfffe434a8b22f815a61ec
Lenny Brown <penn2014@gmail.com>
2021-03-02 18:03:43 -0800

    Hopefully fixed dates and also cleaned up workflow

--- test/ui/ui-tests-user.js
+++ test/ui/ui-tests-user.js
@@ -7,17 +7,18 @@ const puppeteer = require("puppeteer");
     console.log(`Page opened, going to ${uiLocation}`);
     await page.goto(uiLocation)
 
-    let [month, date, year]    = new Date().toLocaleDateString("en-US").split("/")
-    let [hour, minute, second, period] = new Date().toLocaleTimeString("en-US").split(/:| /)
-
-    if (String(month).length == 1) month = "0" + month;
-    if (String(date).length == 1) date = "0" + date;
-    
-    if (period === "PM") hour = Number(hour) + 12;
-
-    if (String(hour) == 1) hour = "0" + hour;
-    if (String(minute).length == 1) minute = "0" + minute;
-    if (String(second).length == 1) minute = "0" + second;
+    const dateOptions = { 
+        timeZone: "America/Los_Angeles",
+        month: '2-digit',
+        day: '2-digit',
+        hour12: false,
+        hour: '2-digit', 
+        minute: '2-digit',
+        second: '2-digit',
+        year: 'numeric'
+    };
+    let [month, date, year]    = new Date().toLocaleDateString("en-US", dateOptions).split(",")[0].split("/");
+    let [_, hour, minute, second] = new Date().toLocaleTimeString("en-US", dateOptions).split(/:| /);
 
     const todate = `${year}_${month}_${date}_${hour}-${minute}-${second}`;
     const screenshotPath = `../results/ui-test-results/users/ui_users_${todate}.png`;
@@ -26,4 +27,4 @@ const puppeteer = require("puppeteer");
     await page.screenshot({ path: screenshotPath});
 
     await browser.close();
-})(); 
+})().catch(err => console.log(err)); 
