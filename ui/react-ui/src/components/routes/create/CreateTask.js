d7a32c2fcc5d5deb59df0f17f01247f26108a255
luca <luca627@comcast.net>
2021-03-02 18:55:49 -0800

    adds axios

--- ui/react-ui/src/components/routes/create/CreateTask.js
+++ ui/react-ui/src/components/routes/create/CreateTask.js
@@ -1,130 +1,122 @@
-import React, { useState, useRef } from 'react';
+
+import axios from 'axios';
+import React, { Component } from 'react';
 import { NavLink } from "react-router-dom";
-import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import './Create.css';
 
-const CreateTask = () => {
 
-    const [firstName, setFirstName] = useState();
-    const [lastName, setLastName] = useState();
-    const [email, setEmail] = useState();
-    const [validated, setValidated] = useState();
-    const [userCreateSuccess, setUserCreateSuccess] = useState(false);
-    const [isVisible, setIsVisible] = useState(false);
 
-    const formRef = useRef(null);
+// const Create = () => {
 
-    const handleChange = (e) => {
-        const fieldName = e.target.name;
-        const fieldValue = e.target.value;
+//     const [firstName, setFirstName] = useState();
+//     const [lastName, setLastName] = useState();
+//     const [email, setEmail] = useState();
+//     const [validated, setValidated] = useState();
+//     const [userCreateSuccess, setUserCreateSuccess] = useState(false);
+//     const [isVisible, setIsVisible] = useState(false);
+// };
 
-        // Save fields to state
-        if (fieldName === 'firstName') {
-            setFirstName(fieldValue);
-        } else if (fieldName === 'lastName') {
-            setLastName(fieldValue);
-        } else if (fieldName === 'email') {
-            setEmail(fieldValue);
+class CreateTask extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             taskId: '',
+             userId: '',
+             title: '',
+             description: '',
+             createdDate: '',
+             duedate: '',
+             completed: '',
+             completedDate: ''
+
         }
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const form = formRef.current;
+    changerHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value })
 
-        // Form is invalid
-        if (form.checkValidity() === false) {
-            setValidated(true);
-        } 
-        // Form is good to go
-        else {
-            await createUser(); // Call function to create user
-            form.reset();
-        }
     }
+    
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+        axios.post('https://jsonplaceholder.typicode.com/posts', this.state)
+        .then(response =>{
+            console.log(response)
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }
+    
 
-    const createUser = async () => {
-        const body = { firstName, lastName, email };
-        await fetch('https://nsc-fun-dev-usw2-thursday.azurewebsites.net/api/users/', {
-            method: 'post', 
-            headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(body)
-        }).then((response) => {
-            setIsVisible(true);
-            // User has been created succsesfully
-            if (response.status === 200 || response.status === 201) { // Can also use created user response userId instead
-                setUserCreateSuccess(true);
-            } 
-            // There was an error creating the user
-            else {
-                setUserCreateSuccess(false);
-            }
-        });
-       
-       
-        //// Alternative if using userId of created user instead of response code
-        // .then((response) => {
-        //     response.json()
-        //         .then((data) => {
-        //             console.log(data.userId)
-        //         })
-    }
+    render() {
+        const { taskId, userId, title, description, createdDate, duedate, completed, completedDate} = this.state
         return (
             <div className="formContainer">
                 <NavLink to={`/`} onClick={null}>
                     <Button variant="outline-primary" size="sm">Home</Button>
                 </NavLink>
-                <Alert show={isVisible} dismissible variant={userCreateSuccess ? "success" : "danger"} onClose={() => setIsVisible(false)}>
-                    {userCreateSuccess ? <Alert.Heading>User Successfully Created</Alert.Heading> : <Alert.Heading>Error Creating User</Alert.Heading> }
-                </Alert>
-                <h2 className='title'>Create Task</h2>
-                <Form noValidate validated={validated} onSubmit={handleSubmit} onChange={handleChange} ref={formRef}>
-                <Form.Group>
-                        <Form.Label>Tasks ID: </Form.Label>
-                        <Form.Control required as="input" type="text" placeholder="##" name="firstName" />
-                        <Form.Control.Feedback type="invalid">
-                            Invalid Task ID
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>User ID: </Form.Label>
-                        <Form.Control required as="input" type="text" placeholder="##" name="firstName" />
-                        <Form.Control.Feedback type="invalid">
-                            Invalid User ID
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Title: </Form.Label>
-                        <Form.Control required as="input" type="text" placeholder="Clean storage"  name="lastName" />
-                        <Form.Control.Feedback type="invalid">
-                            Invalid Title
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Description: </Form.Label>
-                        <Form.Control required type="input" placeholder="Remove Old items, Powerwash"  name="email" />
-                        <Form.Control.Feedback type="invalid">
-                            Invalid description
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Created Date: </Form.Label>
-                        <Form.Control required type="input" placeholder="02/27/2021"  name="email" />
-                        <Form.Control.Feedback type="invalid">
-                            Invalid Date
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Create
-                    </Button>
-                </Form>
+            
+            <div>
+             <form onSubmit={this.submitHandler}>
+                <div>
+                    <input type="text" 
+                    name='taskId' 
+                    value= {taskId}
+                    onChange={this.changerHandler} />
+                </div>
+                <div>
+                    <input type="text" 
+                    name='userId' 
+                    value= {userId}
+                    onChange={this.changerHandler} />
+                </div>
+                <div>
+                    <input type="text" 
+                    name='title' 
+                    value= {title}
+                    onChange={this.changerHandler}  />
+                </div>
+                <div>
+                    <input type="text" 
+                    name='description' 
+                    value= {description}
+                    onChange={this.changerHandler}  />
+                </div>
+                <div>
+                    <input type="text" 
+                    name='createdDate' 
+                    alue= {createdDate}
+                    onChange={this.changerHandler}  />
+                </div>
+                <div>
+                    <input type="text"  
+                    name='duedate' 
+                    value= {duedate}
+                    onChange={this.changerHandler}  />
+                </div>
+                <div>
+                    <input type="text"  
+                    name='completed' 
+                    value= {completed}
+                    onChange={this.changerHandler}  />
+                </div>
+                <div>
+                    <input type="text"  
+                    name='completedDate' 
+                    value= {completedDate}
+                    onChange={this.changerHandler}  />
+                </div>
+                <button type="submit">Submit</button>
+             </form>                
+            </div>
             </div>
         )
+    }
 }
 
-export default CreateTask;
+export default CreateTask
