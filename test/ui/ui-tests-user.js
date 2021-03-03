e4f8cd0ea830cddc78861c879a2cc334067001e5
Lenny Brown <penn2014@gmail.com>
2021-03-02 16:36:24 -0800

    Fixed file structures and added dates to the screenshots

--- test/ui/ui-tests-user.js
+++ test/ui/ui-tests-user.js
@@ -13,8 +13,20 @@ const puppeteer = require("puppeteer");
     //     page.waitForNavigation(),
     //     div.click(),
     // ]);
+    let [month, date, year]    = new Date().toLocaleDateString("en-US").split("/")
+    let [hour, minute, second, period] = new Date().toLocaleTimeString("en-US").split(/:| /)
 
-    await page.screenshot({ path: "../results/ui-test-results/ui-test-users-page-screenshot.png"});
+    if (String(month).length == 1) month = "0" + month;
+    if (String(date).length == 1) date = "0" + date;
+    
+    if (period === "PM") hour = Number(hour) + 12;
+
+    if (String(hour) == 1) hour = "0" + hour;
+    if (String(minute).length == 1) minute = "0" + minute;
+    if (String(second).length == 1) minute = "0" + second;
+
+    todate = `${year}_${month}_${date}_${hour}-${minute}-${second}`;
+    await page.screenshot({ path: `../results/ui-test-results/users/ui_users_${todate}.png`});
 
     await browser.close();
 })(); 
