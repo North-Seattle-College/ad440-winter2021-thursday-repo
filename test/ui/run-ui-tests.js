d563231197f9c77a01bfc8ce39249d48ee4d8142
Sean Gilliland <seang@Seans-MacBook-Pro.local>
2021-03-07 15:04:01 -0800

    Fixes and Finished Ui Tests. Test can now simulate and fire React onChange events as well as propogate programatically changed values to the React state. UI-test runner can loop through an iterable of endpoints to test multiple pages at once.

--- test/ui/run-ui-tests.js
+++ test/ui/run-ui-tests.js
@@ -1,18 +1,35 @@
-import {startBrowser, getSelectorContent} from './ui-test-tasks.js';
+import {startBrowser, goToHomePage, fillInputs, goToEndpointPage, getSelectorContent} from './ui-test-tasks.js';
+import puppeteer from 'puppeteer';
 
-//const baseUrl = 'https://nscstrdevusw2thucommon.z5.web.core.windows.net';
-const baseUrl = 'http://localhost:3000'
-const endPoints = {
-    'users': '/users',
-    'userId': '/users/{userId}',
-    'tasks': '/users/{userId}/tasks',
-    'taskId': '/users/{userId}/tasks/{taskId}', 
+const baseUrl = 'https://nscstrdevusw2thucommon.z5.web.core.windows.net';
+const endPoints = new Map();
+//endPoints.set('users', '/users');
+endPoints.set('userId', '/users/{userId}');
+endPoints.set('tasks', '/users/{userId}/tasks');
+endPoints.set('taskId', '/users/{userId}/tasks/{taskId}');
+
+
+
+/**
+ * The Main UI test runner.
+ * @param {url} - The URL to navigate to.
+ * @param {Iterable} - An iterable of strings representing endpoints.
+ * @param {}
+ * @return {PuppetInstance} - The puppeteer intance
+ */
+const runUiTest = async (url, endpoints, selector) => {
+  let {browser, page} = await startBrowser();
+  let screenshotDir = `../results/ui-test-results/{}${new Date(Date.now()).toISOString()}.png`
+  for (let endpoint of endpoints.values()) {
+    await goToHomePage(url, page);
+    await fillInputs(page, endpoint);
+    await goToEndpointPage(page, endpoint);
+    let selectorContent = await getSelectorContent(page, selector);
+    console.log(`Text Value of header: ${selectorContent}`);
+    let screenshotDir = `../results/ui-test-results/${endpoint.replace(/\//g, '-')}-${new Date(Date.now()).toISOString()}.png`
+    await page.screenshot({path: screenshotDir});
+  };
+  await browser.close();
 };
 
-(async () => {
-    let url = baseUrl;
-    let selector = 'h1';
-    let pupInstance = await startBrowser();
-    const page = pupInstance.page;
-    let selectorContent = await getSelectorContent(url, page, endPoints.tasks, selector);
-})();
+runUiTest(baseUrl, endPoints, 'h1').catch(error => console.error(error));
