97bd57ab96e4455553141c5a7be9adc0d875d768
AllisonBC <allycm07@gmail.com>
2021-03-23 07:15:00 -0700

    Add test files

---
+++ test/ui/test-users-load40-cache-on.js
@@ -0,0 +1,30 @@
+/**
+ * Loads users/ page 40 times (with caching on) capturing a screenshot and load time for each.
+ */
+
+ const puppeteer = require("puppeteer");
+ const testCount = 40;
+ const url = "https://nscstrdevusw2thucommon.z5.web.core.windows.net/#/users";
+
+ (async () => {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  page.setCacheEnabled([true]);
+
+  for (let i = 0; i < testCount; ++i){
+    await page.goto(url);
+    const usersMetrics = await page.metrics();
+    const testDate = usersMetrics.Timestamp
+    const screenshotPath = `../../results/ui-test-results/users/load40_cache_on_${testDate}.png`;
+    const browserTimer = usersMetrics.TaskDuration
+    console.log(
+      `${url}-  
+      Load time test: #${i}, 
+      Time to load page: ${browserTimer}, 
+      Screenshot will be stored as: ${screenshotPath}`);
+    await page.screenshot({ path: screenshotPath});
+  }  
+
+  await browser.close();
+})().catch(err => console.log(err)); 
