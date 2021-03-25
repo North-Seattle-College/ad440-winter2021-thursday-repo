51705dcb53f20e21113df7486c75bd87d4a053e6
Dylan Riley <dylan@rileyz.net>
2021-03-24 22:37:10 -0700

    added back button, TODO comments

--- ui/react-ui/src/components/routes/userTasks/UserTasksPaginated.js
+++ ui/react-ui/src/components/routes/userTasks/UserTasksPaginated.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
+import BackButton from '../../bootstrapBackButton/BootstrapBackButton'
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator'
 import * as ReactBootStrap from 'react-bootstrap';
-import { fetchSetTblState } from '../../../utils';
 import axios from 'axios';
 
 const UserTasksPaginated = () => {
@@ -21,8 +21,17 @@ const UserTasksPaginated = () => {
         {dataField: "completedDate", text: "Date Completed"}
     ]
 
+    // TODO:
+    // Add options to delete a task
+    // sort of present in original UserTasks.js but didn't really work
+
     const getData = async (userId)=>{
         try{
+            /*
+             * I wanted to use the fetchTableState from ../../utils
+             * But it just returned the data in a really unhelpful way with an incorrect
+             * "title" and "subtitle" tacked onto the front  
+             */
             const data = await axios.get(
                 `https://nsc-fun-dev-usw2-thursday.azurewebsites.net/api/users/${userId}/tasks`
             );
@@ -38,8 +47,12 @@ const UserTasksPaginated = () => {
         setLoading(true);
     }, [userId]);
 
+    // TODO:
+    // Table spacing is bad in narrow windows
+
     return (
         <div className="pagination">
+            <div><BackButton /></div>
             {loading
                 ?(
                     <div className="table-wrapper">
@@ -49,6 +62,7 @@ const UserTasksPaginated = () => {
                             keyField="taskId"
                             data={userTasks}
                             columns={columns}
+                            pagination={paginationFactory()}
                         />
                     </div>
                 ): (<ReactBootStrap.Spinner animation="border" />)}
