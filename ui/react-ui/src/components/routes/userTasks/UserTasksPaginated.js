34bb7298c43a3241a8b9e98f4a5e8a58c0f23a6e
Dylan Riley <dylan@rileyz.net>
2021-03-24 22:28:02 -0700

    trying axios implementation like in usersPaginated

--- ui/react-ui/src/components/routes/userTasks/UserTasksPaginated.js
+++ ui/react-ui/src/components/routes/userTasks/UserTasksPaginated.js
@@ -4,26 +4,53 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator'
 import * as ReactBootStrap from 'react-bootstrap';
 import { fetchSetTblState } from '../../../utils';
+import axios from 'axios';
 
 const UserTasksPaginated = () => {
     let [userTasks, setUserTasks] = useState([]);
     let [loading, setLoading] = useState(false);
     let {userId} = useParams();
 
-    useEffect(() => {
+    const columns = [
+        {dataField: "taskId", text: "Task ID"},
+        {dataField: "title", text: "Task"},
+        {dataField: "description", text: "Description"},
+        {dataField: "createdDate", text: "Date Created"},
+        {dataField: "dueDate", text: "Due Date"},
+        {dataField: "completed", text: "Is Complete"},
+        {dataField: "completedDate", text: "Date Completed"}
+    ]
+
+    const getData = async (userId)=>{
         try{
-            const response = fetchSetTblState(`users/${userId}/tasks`, setUserTasks)
-            setLoading(true)
+            const data = await axios.get(
+                `https://nsc-fun-dev-usw2-thursday.azurewebsites.net/api/users/${userId}/tasks`
+            );
+            console.log(data);
+            setUserTasks(data.data);
         } catch (error) {
             console.error(error);
         }
+    }
+
+    useEffect(() => {
+        getData(userId);
+        setLoading(true);
     }, [userId]);
 
     return (
         <div className="pagination">
             {loading
                 ?(
-                    <div><pre>{JSON.stringify(userTasks, null, 5)}</pre></div>
+                    <div className="table-wrapper">
+                        <BootstrapTable
+                            rowStyle = {{backgroundColor: 'white'}}
+                            className="table"
+                            keyField="taskId"
+                            data={userTasks}
+                            columns={columns}
+                        />
+                    </div>
                 ): (<ReactBootStrap.Spinner animation="border" />)}
         </div>
     )
