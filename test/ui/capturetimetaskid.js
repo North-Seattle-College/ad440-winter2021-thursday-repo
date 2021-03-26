0a202aaf4ea7d023ad3529bf20ffff628fad74c9
Char Wae (Erica) Chong <57076488+chonger878@users.noreply.github.com>
2021-03-26 13:43:03 -0700

    Eliminated pagemetrics() method to make it more simple.

--- test/ui/capturetimetaskid.js
+++ test/ui/capturetimetaskid.js
@@ -1,28 +1,24 @@
 const puppeteer = require('puppeteer')
 
 var NUM_TIMES=40;
-const ENV_PAGE = `https://nscstrdevusw2thucommon.z5.web.core.windows.net/#/users/2/tasks/1`;
 let totalSeconds = 0;
 let loadtime=0;
 
-const  pageMetrics = async() =>{
-    const browser = await puppeteer.launch({headless: false})
-    const webpage = await browser.newPage()
-    await webpage.setCacheEnabled([False])
-    ENV_PAGE = os.environ['target_website']
-    await webpage.goto(ENV_PAGE)
-    const pagePerf = await page.evaluate(_=>{
-            loadtime =PerformanceNavigationTiming.loadEventEnd - PerformanceNavigationTiming.loadEventStart
-            return loadtime
-       
-    })
-    totalSeconds+=pagePerf.loadtime
-    console.log(`Page Load took: ${pagePerf.loadtime}ms`)
-    await webpage.close()
-}
+(async() =>{
+    const ENV_PAGE = "https://nscstrdevusw2thucommon.z5.web.core.windows.net/#/users/2/tasks/1";
+    const browser = await puppeteer.launch()
+    for(var i = 0; i< NUM_TIMES; i++){
+        const webpage = await browser.newPage();
+        console.log(`Opening Page. Heading for target page...`);
+        await webpage.goto(ENV_PAGE)
+        await webpage.setCacheEnabled(false)
+        loadtime =PerformanceNavigationTiming.loadEventEnd - PerformanceNavigationTiming.loadEventStart;
+        await webpage.close();    
+        }
+        console.log(`The average time it took to load the page 40 times: ${totalSeconds/NUM_TIMES}`)
+    })().catch(err => console.log(err));
+    
+    
+    
 
-for(var i = 0; i <NUM_TIMES; i++){
-    pageMetrics();
 
-}
-console.log(`The average time it took to load the page 40 times: ${totalSeconds/NUM_TIMES}`)
