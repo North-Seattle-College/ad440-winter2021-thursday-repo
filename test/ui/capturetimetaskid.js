ba24971eecdd15286735811d9f66633653799891
Char Wae (Erica) Chong <57076488+chonger878@users.noreply.github.com>
2021-03-26 16:01:08 -0700

    Deleted PerformanceTimingNavigation and simply swapped it out with Date.

--- test/ui/capturetimetaskid.js
+++ test/ui/capturetimetaskid.js
@@ -5,6 +5,8 @@ let totalSeconds = 0;
 let loadtime=0;
 
 (async() =>{
+    var startDate = new Date();
+    var startTime = startDate.getTime();
     const ENV_PAGE = "https://nscstrdevusw2thucommon.z5.web.core.windows.net/#/users/2/tasks/1";
     const browser = await puppeteer.launch()
     for(var i = 0; i< NUM_TIMES; i++){
@@ -12,7 +14,9 @@ let loadtime=0;
         console.log(`Opening Page. Heading for target page...`);
         await webpage.goto(ENV_PAGE)
         await webpage.setCacheEnabled(false)
-        loadtime =PerformanceNavigationTiming.loadEventEnd - PerformanceNavigationTiming.loadEventStart;
+            var endDate = new Date();
+        loadtime = endDate.getTime-startTime;
+        console.log(`Time to load page was: ${loadtime}`)
         await webpage.close();    
         }
         console.log(`The average time it took to load the page 40 times: ${totalSeconds/NUM_TIMES}`)
