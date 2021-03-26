8ec01c821cdb6be922b2fdc595d86359e22d3a9b
natni <44041437+natni@users.noreply.github.com>
2021-03-25 18:52:30 -0700

    Disabled cache on browser

--- test/ui/time-users-id-tasks.js
+++ test/ui/time-users-id-tasks.js
@@ -2,17 +2,16 @@ const puppeteer = require("puppeteer");
 var captureTime = 40;
 (async () => {
     const uiLocation = "https://nscstrdevusw2thucommon.z5.web.core.windows.net/#/users/6/tasks ";
-    const browser = await puppeteer.launch();   
+    const browser = await puppeteer.launch(); 
     var startDate = new Date();
     var totalSeconds = 0;     
     var startTime = startDate.getTime();
 
     for(var i=0; i<captureTime; i++){
         const page = await browser.newPage();
-        console.log(`Page opened, going to ${uiLocation}`);
-
+        console.log(`Page opened, going to ${uiLocation}`);  
         await page.goto(uiLocation)
-
+        await page.setCacheEnabled(false)  
         console.log(`Navigated to ${uiLocation}, taking screenshot`);
             await page.screenshot();
             var endDate   = new Date();
